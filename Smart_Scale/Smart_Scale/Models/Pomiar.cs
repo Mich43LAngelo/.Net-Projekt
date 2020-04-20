9d274cddf45ac105ad85c33728131e823c78e7dd
Bialy <mat327@interia.pl>
2020-04-20 17:28:03 +0200

    Etap 1 projektu

---
+++ Smart_Scale/Smart_Scale/Models/Pomiar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Smart_Scale.Models
+{
+    public class Pomiar
+    {
+        public int Id { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Pole Waga jest wymagane.")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:0,0.00}")]
+        public decimal Waga { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Pole Data dodania jest wymagane.")]
+        [System.ComponentModel.DataAnnotations.DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:MM}", ApplyFormatInEditMode = true)]
+        public DateTime Datadodania  { get; set; }
+        public virtual User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
